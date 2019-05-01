47724dc7b7ad5a70bbc0a7b888cd85a7ce3e707a
Rita <ritkapuh1@gmail.com>
2019-05-01 11:55:00 +0300

    Filter was implemented

--- js/phones/phones-page.js
+++ js/phones/phones-page.js
@@ -2,23 +2,26 @@ import PhonesCatalog from './components/phones-catalog.js';
 import PhonesService from './services/phones-service.js';
 import PhoneViewer from './components/phone-viewer.js';
 import ShoppingCart from './components/shopping-cart.js';
+import Filter from './components/filter.js';
 
 export default class PhonesPage {
   constructor({ element }) { 
     this._element = element;
     this._render();
     
+    this._initFilter();
     this._initCatalog();
     this._initViewer();
     this._initCart();
-  
+    
   }
 
   _initCatalog() {
     this._catalog = new PhonesCatalog({
-      element: this._element.querySelector('[data-component="phone-catalog"]'),
-      phones: PhonesService.getAll()
+      element: this._element.querySelector('[data-component="phone-catalog"]')
     })
+
+    this._showPhones();
 
     this._catalog.subscribe('phone-selected', (id) =>{
       console.log('Selected: ', id);
@@ -39,7 +42,7 @@ export default class PhonesPage {
     })
 
     this._viewer.subscribe('back', () => {
-       this._catalog.show();
+       this._showPhones();
        this._viewer.hide();
     })
 
@@ -54,6 +57,29 @@ export default class PhonesPage {
     })
   }
 
+  _initFilter() {
+    this._filter = new Filter({
+      element: this._element.querySelector('[data-component="filter"]')
+    })
+    
+    this._filter.subscribe('query-change', (eventData) => {
+     this._showPhones();
+    })
+
+    this._filter.subscribe('order-change', (eventData) => {
+      this._showPhones();
+    })
+
+  }
+  
+  _showPhones() {
+     this._currentFiltering = this._filter.getCurrent();
+     const phones = PhonesService.getAll(this._currentFiltering);
+     console.log('showing phones by criteria:', this._currentFiltering);
+     this._catalog.show(phones);
+
+  }
+
   _render() {
       this._element.innerHTML = `
       <div class="row">
@@ -61,18 +87,7 @@ export default class PhonesPage {
       <!--Sidebar-->
       <div class="col-md-2">
         <section>
-          <p>
-            Search:
-            <input>
-          </p>
-
-          <p>
-            Sort by:
-            <select>
-              <option value="name">Alphabetical</option>
-              <option value="age">Newest</option>
-            </select>
-          </p>
+          <div data-component = "filter"></div>
         </section>
 
         <section>
